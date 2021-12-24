bde3252e49c020b5644254a831a98c527ffc3994
desrilfatra <desrilfatra8@gmail.com>
2021-12-24 19:21:51 +0700

    details

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [FrontendController::class, 'index'])->name('index');
+Route::get('/detail/{slug}', [FrontendController::class, 'details'])->name('details');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
