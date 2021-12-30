1b54f13242972774b2d89b2fa18f92e6627fe340
desrilfatra <desrilfatra8@gmail.com>
2021-12-30 20:31:18 +0700

    create photos

--- resources/views/pages/dashboard/gallery/create.blade.php
+++ resources/views/pages/dashboard/gallery/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Product &raquo; Create
+            Product &raquo; {{ $product -> name }} &raquo; Upload Photos
         </h2>
     </x-slot>
  
@@ -24,30 +24,18 @@
                       </div>
                   </div>
               @endif
-              <form action="{{ route('dashboard.product.store') }}" class="w-full" method="post" enctype="multipart/form-data">
+              <form action="{{ route('dashboard.product.gallery.store' , $product->id) }}" class="w-full" method="post" enctype="multipart/form-data">
                     @csrf
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3 ">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Name</label>
-                            <input type="text" value="{{ old('name') }}" name="name" placeholder="Product Name" class="block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                        </div>
-                    </div>
-                    <div class="flex flex-wrap -mx-3 mb-6">    
-                        <div class="w-full px-3 ">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Description</label>
-                            <textarea name="description" class="block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">{!! old('description') !!}</textarea>
-                        </div>
-                    </div>
-                    <div class="flex flex-wrap -mx-3 mb-6">
-                        <div class="w-full px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Price</label>
-                            <input type="number" value="{{ old('price') }}" name="price" placeholder="Product Price" class="block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <input type="file" multiple name="files[]" accept="image/*" placeholder="Photos" class="block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3">
                         <button type="submit" class="bg-green-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded shadow-lg ">
-                            Save Product    
+                            Save Gallery    
                         </button>   
                         </div>
                     </div>
