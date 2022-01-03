a3542f690824d66fc33e1d25a6f84f22bba2f73b
desrilfatra <desrilfatra8@gmail.com>
2022-01-03 20:58:07 +0700

    mytransaction route

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -29,6 +29,11 @@
                         {{ __('User') }}
                     </x-jet-nav-link>
                     @endif
+
+                    <x-jet-nav-link href="{{ route('dashboard.my-transaction.index') }}" :active="request()->routeIs('dashboard.my-transaction.index')">
+                        {{ __('My Transaction') }}
+                    </x-jet-nav-link>
+
                 </div>
             </div>
 
@@ -170,6 +175,9 @@
             </x-jet-nav-link>
             @endif
 
+            <x-jet-nav-link href="{{ route('dashboard.my-transaction.index') }}" :active="request()->routeIs('dashboard.my-transaction.index')">
+                {{ __('My Transaction') }}
+            </x-jet-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
