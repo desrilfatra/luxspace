9883a1c6885bc17f37b8778b574161d89079c663
desrilfatra <desrilfatra8@gmail.com>
2022-01-17 10:22:19 +0700

    modify use mytransaction

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductGalleryController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\MyTransactionController;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Route;
 
